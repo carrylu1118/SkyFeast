aa837633dd4e936d89edb386c0b5c83e1e7ca534
carrylu <carryluemail@gmail.com>
2026-06-03 20:38:37 +0800

    营业额数据统计功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -58,4 +59,10 @@ public interface OrderMapper {
      */
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 根据动态条件查询数据
+     * @param map 查询条件
+     */
+    Double sumByMap(Map map);
 }
